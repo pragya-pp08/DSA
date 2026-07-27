cdb7e3749b70c34fb581c51da0c29ed7810b9895
Pragya Pandey <pragya.r.pandey08@gmail.com>
2026-07-28 00:17:24 +0530

    Time: 3 ms (81.01%), Space: 43.5 MB (66.66%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.java
+++ 0682-baseball-game/0682-baseball-game.java
@@ -1,44 +1,32 @@
 class Solution {
     public int calPoints(String[] operations) {
          Stack<Integer> st = new Stack<>();
-
-        for (int i = 0; i < operations.length; i++) {
-
-            if (operations[i].equals("+")) {
-
-                int first = st.pop();      // Top score
-                int second = st.peek();    // Second top score
-
-                st.push(first);            // Put the top back
-                st.push(first + second);   // Push their sum
-
-            }
-
-            else if (operations[i].equals("D")) {
-
-                st.push(st.peek() * 2);
-
-            }
-
-            else if (operations[i].equals("C")) {
-
+         for(int i=0;i<operations.length;i++){
+            
+            if(operations[i].equals("C")){
                 st.pop();
-
             }
-
-            else {
-
+            else if(operations[i].equals("D")){
+                st.push(st.peek()*2);
+            }
+            else if(operations[i].equals("+")){
+                int first=st.peek();
+                st.pop();
+                int second=st.peek();
+                st.push(first);
+                st.push(first+second);
+            }
+            else{
                 st.push(Integer.parseInt(operations[i]));
-
             }
         }
-
-        int sum = 0;
-
-        while (!st.isEmpty()) {
-            sum += st.pop();
+        int sum=0;
+        while(!st.empty()){
+            sum=sum+st.peek();
+            st.pop();
         }
-
         return sum;
+         
+
     }
 }
