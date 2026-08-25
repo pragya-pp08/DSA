f50e0d2a7132065e79164bf3a2ee0277d05539e8
Pragya Pandey <pragya.r.pandey08@gmail.com>
2026-08-26 00:35:19 +0530

    Time: 1 ms (22.79%), Space: 48.2 MB (55.53%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,11 +1,15 @@
 class Solution {
     public void reverseString(char[] s) {
-        Stack<Character>st=new Stack<>();
-        for(int i=0;i<s.length;i++){
-            st.push(s[i]);
-        }
-        for(int i=0;i<s.length;i++){
-            s[i]=st.pop();
-        }
+        int i=0;
+       int  j=s.length-1;
+       while(i<=j){
+        char temp=s[i];
+        s[i]=s[j];
+        s[j]=temp;
+        i++;
+        j--;
+       }
+       
+
     }
 }
